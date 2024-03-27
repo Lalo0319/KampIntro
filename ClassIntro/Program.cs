eef8ca1dd3d8529c1b70925a0d962b1311a6cdcc
Lalo0319 <aykacmehmetbugra03@gmail.com>
2024-03-27 15:22:47 +0300

    Ders sonu

--- ClassIntro/Program.cs
+++ ClassIntro/Program.cs
@@ -4,46 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
             Kurs kurs1 = new Kurs();
-            kurs1.KursAdı = "C#";
-            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.KursAdi = "C#";
+            kurs1.EgitmenAdı = "Engin Demiroğ";
             kurs1.IzlenmeOrani = 68;
 
             Kurs kurs2 = new Kurs();
-            kurs2.KursAdı = "Java";
-            kurs2.Egitmen = "Kerem Varış";
-            kurs2.IzlenmeOrani = 80;
+            kurs2.KursAdi = "Java";
+            kurs2.EgitmenAdı = "Kerem Varış";
+            kurs2.IzlenmeOrani = 64;
 
             Kurs kurs3 = new Kurs();
-            kurs3.KursAdı = "Python";
-            kurs3.Egitmen = "Berkay Bilgin";
-            kurs3.IzlenmeOrani = 75;
+            kurs3.KursAdi = "Python";
+            kurs3.EgitmenAdı = "Berkay Bilgin";
+            kurs3.IzlenmeOrani = 80;
 
-            Kurs[] kurslar = new Kurs[] {kurs1,kurs2,kurs3 };
-            foreach (Kurs kurs in kurslar)
+            Kurs[]kurslar = new Kurs[] { kurs1, kurs2,kurs3 };
+            foreach(var kurs in kurslar)
             {
-                Console.WriteLine(kurs.KursAdı+" : "+kurs.Egitmen+" : "+kurs.IzlenmeOrani);
+                Console.WriteLine(kurs.KursAdi+" : "+kurs.EgitmenAdı+" : "+kurs.IzlenmeOrani);
             }
-            for (int i = 0; i < kurslar.Length; i++)
-            {
-                Kurs kurs = kurslar[i];
-                Console.WriteLine(kurs.KursAdı + " : " + kurs.Egitmen + " : " + kurs.IzlenmeOrani);
-            }
-
 
         }
 
 
 
     }
-
     class Kurs
     {
-        public string KursAdı { get; set; }
-        public string Egitmen { get; set; }
+        public string EgitmenAdı { get; set; }
+        public string KursAdi { get; set; }
         public int IzlenmeOrani { get; set; }
-
     }
+    
+    
 }
